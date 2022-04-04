6c6047372c3de9fc5015a7006a360a4423cdfbee
Afonina-Olga <olga.g.afonina>
2022-04-04 18:24:16 +0300

    Тесты для CommandLineOptions

--- ArchitectureByDisruptorPattern.Tests/CommandLineOptionsTests.cs
+++ ArchitectureByDisruptorPattern.Tests/CommandLineOptionsTests.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 
 namespace ArchitectureByDisruptorPattern.Tests
 {
-	public class Tests
+	public class CommandLineOptionsTests
 	{
-		[SetUp]
-		public void Setup()
-		{
-		}
-
 		[Test]
 		public void TryParse_InputIsValid_ReturnValuesCollection()
 		{
@@ -17,10 +12,67 @@ namespace ArchitectureByDisruptorPattern.Tests
 			var arguments = new string[] { "1,Sum,1,2", "2,Diff,5,4", "3,Mult,2,3" };
 
 			// Act
-			var result = CommandLineOptions.TryParse(arguments, out List<CommandLineParsedInput> parsedInput);
+			var result = TryParseCommandLineArguments(arguments);
 
 			// Assert
 			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void TryParse_InvalidFormatCommand_ReturnFalse()
+		{
+			// Arrange
+			var arguments = new string[] { "absd", "2,Diff,5,4", "3,Mult,2,3" };
+
+			// Act
+			var result = TryParseCommandLineArguments(arguments);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void TryParse_WrongCommandNameWithCorrectFormat_ReturnValuesCollection()
+		{
+			// Arrange
+			var arguments = new string[] { "1,Add,5,4", "2,Remove,5,4", "3,Substract,2,3" };
+
+			// Act
+			var result = TryParseCommandLineArguments(arguments);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void TryParse_WrongNumberOfArguments_ReturnFalse()
+		{
+			// Arrange
+			var arguments = new string[] { "1,Add,5,4", "2,Remove,5,4" };
+
+			// Act
+			var result = TryParseCommandLineArguments(arguments);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void TryParse_WrongNumberOfCommandArguments_ReturnFalse()
+		{
+			// Arrange
+			var arguments = new string[] { "1,Sum,5,4", "2,Diff,5,4", "3,Mult5,4" };
+
+			// Act
+			var result = TryParseCommandLineArguments(arguments);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+
+		private bool TryParseCommandLineArguments(string[] arguments)
+		{
+			return CommandLineOptions.TryParse(arguments, out List<CommandLineParsedInput> parsedInput);
 		}
 	}
 }
